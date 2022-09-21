4ac1c5538cb0ae12ce0489904e2db451e3fbed42
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-09-21 15:49:55 -0400

    changes to markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,41 +51,41 @@ function generateMarkdown(data) {
   return `
   # ${data.title}
 
-  # License:
+  ## License:
   ${data.license === 
     "Apache 2.0" ? "Apache 2.0" + "" + '<br>' + "" + "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : data.license === "MIT" ? "MIT" + "" + '<br>' + "" + "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : data.license === "IBM" ? "IBM" + "" + '<br>' + "" + "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)" : "Perl" + "" + '<br>' + "" + "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"}
 
 
   ## Table of Contents:
-  ### - [License](#license)
-  ### - [Installation](#installation)
-  ### - [Usage](#usage)
-  ### - [Contribution Guidlines](#contributors)
-  ### - [Tests](#tests)
-  ### - [Questions](#questions)
+  #### - [License](#license)
+  #### - [Installation](#installation)
+  #### - [Usage](#usage)
+  #### - [Contribution Guidlines](#contributors)
+  #### - [Tests](#tests)
+  #### - [Questions](#questions)
 
-  # Description:
-  ### ${data.description}
+  ## Description:
+  #### ${data.description}
 
   ## Installation Process:
-  ### Must install for this app:
+  #### Must install for this app:
   ### ${data.installation}
 
   ## Usage Info:
-  ### ${data.usage}
+  #### ${data.usage}
 
   ## Contibutor Guidlines:
-  ### ${data.contributors}
+  #### ${data.contributors}
 
   ## Test Instructions:
-  ### Run the following commands in your terminal to test this app:
-  ### ${data.tests}
+  #### Run the following commands in your terminal to test this app:
+  #### ${data.tests}
 
   ## Questions: 
-  ### If you have questions contact me:
-  ### GitHub: https://github.com/${data.ghub}
-  ### or
-  ### Email: ${data.email}
+  #### If you have questions contact me:
+  #### GitHub: https://github.com/${data.ghub}
+  #### or
+  #### Email: ${data.email}
 `;
 }
 
